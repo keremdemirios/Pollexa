ab69aec1906fab8a91e27d43ddf4a04e3f5313d5
Kerem Demir <keremdemir1694@gmail.com>
2024-05-26 21:38:22 +0300

    Created PostCell files.

---
+++ Pollexa/Core/Home/Cell/PostCell.swift
@@ -0,0 +1,19 @@
+//
+//  PostCell.swift
+//  Pollexa
+//
+//  Created by Kerem Demir on 26.05.2024.
+//
+
+import UIKit
+
+final class PostCell: UICollectionViewCell {
+
+    static let reuseIdentifier = "PostCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
