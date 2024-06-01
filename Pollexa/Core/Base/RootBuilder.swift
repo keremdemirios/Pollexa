fcaf7a1f4911a57fc391f87d0081c1add21793bc
Kerem Demir <keremdemir1694@gmail.com>
2024-06-01 04:56:10 +0300

    Updated root builder.

--- Pollexa/Core/Base/RootBuilder.swift
+++ Pollexa/Core/Base/RootBuilder.swift
@@ -10,14 +10,6 @@ import UIKit
 final class RootBuilder {
     static func build() -> UINavigationController {
         let navController = UINavigationController(rootViewController: HomeViewController())
-//        let appearance = UINavigationBarAppearance()
-//        appearance.titleTextAttributes = [
-//            .font: UIFont.boldSystemFont(ofSize: 24),
-//            .foregroundColor: UIColor.tintColor
-//        ]
-//        navController.navigationBar.standardAppearance = appearance
-//        navController.navigationBar.scrollEdgeAppearance = appearance
-//        navController.navigationBar.tintColor = .tintColor
         return navController
     }
 }
